44d19d7fbedf180dfb3febb4e2d4225293372e85
isaque Alcantara <alcantara.isaque167@gmail.com>
2023-10-26 16:25:31 -0300

    implement galery to galery component

--- components/galery/index.tsx
+++ components/galery/index.tsx
@@ -1,20 +1,19 @@
 "use client"
 
-import { useState } from "react"
+import { useGalery } from "@/context/galery"
 
 export function Galery() {
-  const [images, setImage] = useState([])
+  const { galery } = useGalery()
 
   return (
     <div className="w-full grid grid-cols-2 gap-2 p-4">
-      <div className="w-full h-[200px] bg-zinc-600 rounded-lg" />
-      <div className="w-full h-[200px] bg-zinc-600 rounded-lg" />
-      <div className="w-full h-[200px] bg-zinc-600 rounded-lg" />
-      <div className="w-full h-[200px] bg-zinc-600 rounded-lg" />
-      <div className="w-full h-[200px] bg-zinc-600 rounded-lg" />
-      <div className="w-full h-[200px] bg-zinc-600 rounded-lg" />
-      <div className="w-full h-[200px] bg-zinc-600 rounded-lg" />
-      <div className="w-full h-[200px] bg-zinc-600 rounded-lg" />
+      {
+        galery.map(image => (
+          <div className="w-full h-[200px] bg-zinc-600 rounded-lg overflow-hidden">
+            <img className="w-full" src={image.base64}/>
+          </div>
+        ))
+      }
     </div>
   )
 }
