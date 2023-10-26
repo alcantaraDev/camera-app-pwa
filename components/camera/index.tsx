d93aa1ca4865007a39b2fe7ec7f03c6396be133d
isaque Alcantara <alcantara.isaque167@gmail.com>
2023-10-26 15:55:29 -0300

    add inital camera component

--- components/camera/index.tsx
+++ components/camera/index.tsx
@@ -1,21 +1,52 @@
 "use client"
 
 import { CameraPlus } from "@phosphor-icons/react"
+import { ChangeEvent } from "react"
 
 export function Camera() {
+  const handleUpload = async (event: ChangeEvent<HTMLInputElement>) => {
+    const input = event.target   
+
+    if (!input || !input.files || input.files.length <= 0) {
+      return
+    }
+
+    const file = input.files[0]
+    const fileReader = new FileReader()
+
+    if (file && fileReader) {
+      fileReader.readAsDataURL(file)
+      fileReader.onload = () => {
+        const base64Image = fileReader.result
+        // enviar a imagem para o contexto e limpar o arquivo selecionado
+        console.log("base64 image: ", base64Image)
+      }
+
+      return
+    }
+    
+    alert("imagem invalida")
+  }
+
   return (
     <>
       <div
       className="fixed bottom-4 w-full px-2"
       >
-        <button
+        {/* <button
         className="
         bg-sky-500 w-full h-[50px] rounded-lg
         flex justify-center items-center
         ">
           <CameraPlus size={24}/>
-        </button>
-      </div>
+        </button> */}
+
+        <input 
+        type="file" 
+        accept="image/x-png,image/jpeg,image/gif"
+        onChange={handleUpload}
+        />
+      </div>  
     </>
   )
 }
