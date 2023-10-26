edc5693340ad5a634575d9b13457328ed167f7a3
isaque Alcantara <alcantara.isaque167@gmail.com>
2023-10-26 16:25:00 -0300

    add galery and addImage to galeryContext

--- context/galery/index.tsx
+++ context/galery/index.tsx
@@ -1,18 +1,31 @@
 "use client"
 
-import { createContext, useContext } from "react";
+import { image } from "@/entities/image";
+import { createContext, useContext, useState } from "react";
 
-type galeryContext = {}
+type galeryContext = {
+  galery: image[]
+  addImage: (image:image) => void
+}
 
-const GaleryContext = createContext({})
+const GaleryContext = createContext<galeryContext>({
+  galery: [],
+  addImage: () => {}
+})
 
 type GaleryProviderProps = {
   children: React.ReactNode
 }
 
 export function GaleryProvider({ children }:GaleryProviderProps) {
+  const [galery, setGalery] = useState<image[]>([])
+
+  const addImage = (image:image) => {
+    setGalery(previous => [...previous, image])
+  }
+
   return (
-    <GaleryContext.Provider value={{}}>
+    <GaleryContext.Provider value={{ galery, addImage }}>
       {children}
     </GaleryContext.Provider>
   )
