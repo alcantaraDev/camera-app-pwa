3ff69dd50d669cc3c21bb5c329b8c7634b7f90a7
isaque Alcantara <alcantara.isaque167@gmail.com>
2023-10-26 16:40:03 -0300

    implement id to galery key

--- components/galery/index.tsx
+++ components/galery/index.tsx
@@ -9,7 +9,7 @@ export function Galery() {
     <div className="w-full grid grid-cols-2 gap-2 p-4">
       {
         galery.map(image => (
-          <div key={image.base64} className="w-full h-[200px] bg-zinc-600 rounded-lg overflow-hidden">
+          <div key={image.id} className="w-full h-[200px] bg-zinc-600 rounded-lg overflow-hidden">
             <img className="w-full" src={image.base64}/>
           </div>
         ))
